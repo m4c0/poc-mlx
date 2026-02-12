02d4061964fcb4f45451dc7faae0c19d913a0e13
Eduardo Costa <m4c0@users.noreply.github.com>
2026-02-12 12:13:49 +0000

    example of logits in loop

---
+++ logits-loop.py
@@ -0,0 +1,19 @@
+import mlx.core as mlx
+from mlx_lm import load
+from mlx_lm.models.cache import KVCache
+
+model, tkz = load("mlx-community/Llama-3.2-3B-Instruct-4bit")
+cache = [KVCache() for _ in range(len(model.layers))]
+
+prompt = "The capital of Paraiba is"
+print(prompt, end="", flush=True)
+
+tokens = mlx.array(tkz.encode(prompt))[None]
+
+while True:
+    logits = model(tokens, cache=cache)
+    nxt = mlx.argmax(logits[:, -1, :], axis=-1)
+    if nxt.item() == tkz.eos_token_id: break
+
+    print(tkz.decode(nxt.tolist()), end="", flush=True)
+    tokens = nxt[None]
