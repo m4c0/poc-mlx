36187aba5c90ff96bff4dfd1e3f8713b18e0aebf
Eduardo Costa <m4c0@users.noreply.github.com>
2026-05-11 11:16:03 +0100

    lora: tweaking to my needs

--- lora-conv.py
+++ lora-conv.py
@@ -1,4 +1,17 @@
 import mlx.core as mx
+
 adapters = mx.load("sentiment-adapter/adapters.safetensors")
-adapters_bf16 = {k: v.astype(mx.bfloat16) for k, v in adapters.items()}
-mx.save_safetensors("sentiment-adapter/adapters.safetensors", adapters_bf16)
+
+# This converts from MLX standard:
+# - F32 to BF16
+# - The stride of each tensor (i.e. transposes them)
+converted = {}
+for name, arr in adapters.items():
+    arr = arr.astype(mx.bfloat16)
+    if ".lora_a" in name or ".lora_b" in name:
+        arr = arr.T
+    converted[name] = arr
+mx.save_safetensors(
+    "sentiment-adapter/adapters-peft.safetensors",
+    converted
+)
--- lora-train.sh
+++ lora-train.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
+set -e
 
 # https://huggingface.co/datasets/yuncongli/chat-sentiment-analysis
 
 time python -m mlx_lm lora \
   --model HuggingFaceTB/SmolLM2-135M \
   --train \
+  --config lora-config.yaml \
   --data ./sentiment \
   --adapter-path ./sentiment-adapter \
   --iters 500 \
   --batch-size 4 \
   --learning-rate 1e-5 \
   --steps-per-report 10
+
+# F32 to BF16
+python lora-conv.py
